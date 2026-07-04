07cfa638f11697cf3a07301350849965fb557c8b
Diakudze <dykovrnd@yandex.ru>
2026-07-04 22:33:21 +0300

    second

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Компиляция приложения с иконкой...
 
-c:\Windows\Microsoft.NET\Framework\v3.5\csc.exe /win32icon:app.ico /out:1C_Launcher.exe Program.cs
+c:\Windows\Microsoft.NET\Framework\v3.5\csc.exe /win32icon:app.ico /out:1C_Launcher_v0.94.exe Program.cs
 
 if %errorlevel% equ 0 (
     echo.
